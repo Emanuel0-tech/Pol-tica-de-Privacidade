40b4847c7470ac4fa94e8a5c6c1f249e1bf71533
Nunnes11 <frannes09@gmail.com>
2025-04-11 16:43:58 -0300

    versão inical

---
+++ Downloads/Vem pra Cá (APP)/mobile/src/components/BaseCard/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+const BaseButtom = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>BaseButtom</Text>
+    </View>
+  );
+};
+
+export default BaseButtom;
